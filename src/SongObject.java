44d3bf489b59a2f8e8dc3c65e24f3bdca0224742
Tomiscout <tomiscout@gmail.com>
2015-06-26 23:51:46 +0300

    Initial

---
+++ src/SongObject.java
@@ -0,0 +1,33 @@
+
+public class SongObject {
+
+	private String name;
+	private String length;
+	
+	public SongObject(String name, int songs, String length){
+		this.name = name;
+		this.length = length;
+	}
+	public SongObject(){
+		this.name = "";
+		this.length = "";
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLength() {
+		return length;
+	}
+	public void setLength(String length) {
+		this.length = length;
+	}
+	
+
+	
+	
+}
