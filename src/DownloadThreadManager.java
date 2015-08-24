7a5793f307b4c83ddf58d5bdfe5c8d3d804ce58a
Tomas Gužauskas <tomiscout@gmail.com>
2015-08-24 18:10:23 +0300

    add DownloadThreadManager
    Organises downloads and download threads

---
+++ src/DownloadThreadManager.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.util.Vector;
+
+public class DownloadThreadManager {
+	static Vector<DownloadTask> downloadTasks = new Vector<DownloadTask>();
+
+	public static void addToQueue(String videoId, File dir){
+		
+	}
+	
+	public static void addToQueue(String playlistId, File dir, boolean sync){
+		
+	}
+	
+	
+	static class DownloadTask{
+		public DownloadTask(String videoId, File dir){
+			
+		}
+	}
+}
+
+
