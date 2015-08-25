292cedfe6c17f975334d5a555372ed4ce1a66907
Tomas Gužauskas <tomiscout@gmail.com>
2015-08-25 03:54:03 +0300

    added YoutubeInMp4 source parser

--- src/YtDownloadUtils.java
+++ src/YtDownloadUtils.java
@@ -14,17 +14,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import com.github.axet.vget.VGet;
-import com.github.axet.vget.info.VGetParser;
-import com.github.axet.vget.info.VideoInfo;
-import com.github.axet.vget.info.VideoInfo.States;
-import com.github.axet.vget.vhs.VimeoInfo;
-import com.github.axet.vget.vhs.YouTubeMPGParser;
-import com.github.axet.vget.vhs.YouTubeQParser;
-import com.github.axet.vget.vhs.YoutubeInfo;
-import com.github.axet.vget.vhs.YoutubeInfo.YoutubeQuality;
-import com.github.axet.wget.info.DownloadInfo;
-import com.github.axet.wget.info.DownloadInfo.Part;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -34,20 +23,16 @@ import com.google.api.services.youtube.model.Playlist;
 import com.google.api.services.youtube.model.PlaylistItem;
 import com.google.api.services.youtube.model.PlaylistItemListResponse;
 
-public class YoutubeDownloadManager {
+public class YtDownloadUtils {
 
-	static public final int SOURCEHEADER = 22000;// aproximate number of chars
-													// to skip
-	static public final String WEBSITEHEADER = "http://keepvid.com/?url=";
+	static public final String YTBINMP4HEADER = "http://youtubeinmp4.com/youtube.php?video=";
+	static public final String KEEPVIDHEADER = "http://keepvid.com/?url=";
 	static public final String YOUTUBEHEADER = "https://www.youtube.com/watch?v=";
 	static public final String YOUTUBESHORTHEADER = "https://youtu.be/";
 
 	private static YouTube youtube;
 	static private final String APIKEY = "AIzaSyBMKWJqJfaRnaZP9KHdSFJJmqrXsrDOe9k";
 
-	// VGet
-	static private VideoInfo info;
-	static private long last;
 
 	// checks link and put to queue
 	public static void parseLink(String s) {
@@ -96,73 +81,23 @@ public class YoutubeDownloadManager {
 			e.printStackTrace();
 		}
 		// downloadVideo(s);
-		getVideosFromPlaylist("PLjNnP__MSqn3cARtTz189JPtPNSFcJKAH");
+		//getVideosFromPlaylist("PLjNnP__MSqn3cARtTz189JPtPNSFcJKAH");
 	}
-
-	private static void downloadVideo(String link) {
+	
+	public static URL getYoutubeUrlFromId(String videoId){
+		URL link = null;
 		try {
-			String source = getUrlSource(new URL(WEBSITEHEADER + link));
-			if (source != null) {
-				URL M4ALink = extractM4AFromSource(source);
-				System.out.println("source link: " + M4ALink);
-				try {
-					saveUrl(new File("M:\\test.m4a"), M4ALink);
-					System.out.println("Ended download");
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-			} else {
-				System.out.println("Failed to get page source.");
-			}
-
-		} catch (IOException e) {
+			link = new URL(YOUTUBEHEADER+videoId);
+		} catch (MalformedURLException e) {
+			System.out.println("Couldn't create Youtube URL");
 			e.printStackTrace();
 		}
+		return link;
 	}
-
-	public static void saveUrl(final File file, final URL url) throws IOException {
-		BufferedInputStream in = null;
-		FileOutputStream fout = null;
-		try {
-			in = new BufferedInputStream(url.openStream());
-			fout = new FileOutputStream(file);
-
-			final byte data[] = new byte[1024];
-			int count;
-			while ((count = in.read(data, 0, 1024)) != -1) {
-				fout.write(data, 0, count);
-			}
-		} finally {
-			if (in != null) {
-				in.close();
-			}
-			if (fout != null) {
-				fout.close();
-			}
-		}
-	}
-
-	private static URL extractM4AFromSource(String source) {
+	
+	public static URL extractM4AFromSource(String source) {
 		URL link = null;
-		int linkPlace = source.indexOf(" Download M4A ");
-		if (linkPlace != -1) {
-			String sub = source.substring(SOURCEHEADER, linkPlace);
-			if (sub != null) {
-				try {
-					link = new URL(sub.substring(sub.lastIndexOf("href=\"") + 6, sub.lastIndexOf("\" class=")));
-				} catch (MalformedURLException e) {
-					System.out.println("Couldn't create download URL");
-					e.printStackTrace();
-				}
-			} else {
-				System.out.println("SOURCEHEADER is in wrong place or bad source");
-			}
-
-		} else {
-			System.out.println("Bad website source! Can't find link place!");
-			YoutubeDownloaderUI.WriteInfo("Bad website source! Can't find link place!");
-		}
+		
 
 		if (link == null) {
 			System.out.println("Couldn't create final dl link");
@@ -170,116 +105,8 @@ public class YoutubeDownloadManager {
 		return link;
 	}
 
-	// Takes correct url and downloads to directory
-	private static void download(URL url, File path) {
-		try {
-			AtomicBoolean stop = new AtomicBoolean(false);
-			Runnable notify = new Runnable() {
-				@Override
-				public void run() {
-					VideoInfo i1 = info;
-					DownloadInfo i2 = i1.getInfo();
-
-					// notify app or save download state
-					// you can extract information from DownloadInfo info;
-					switch (i1.getState()) {
-					case EXTRACTING:
-					case EXTRACTING_DONE:
-					case DONE:
-						if (i1 instanceof YoutubeInfo) {
-							YoutubeInfo i = (YoutubeInfo) i1;
-							System.out.println(i1.getState() + " " + i.getVideoQuality());
-						} else if (i1 instanceof VimeoInfo) {
-							VimeoInfo i = (VimeoInfo) i1;
-							System.out.println(i1.getState() + " " + i.getVideoQuality());
-						} else {
-							System.out.println("downloading unknown quality");
-						}
-						break;
-					case RETRYING:
-						System.out.println(i1.getState() + " " + i1.getDelay());
-						break;
-					case DOWNLOADING:
-						long now = System.currentTimeMillis();
-						if (now - 1000 > last) {
-							last = now;
-
-							String parts = "";
-
-							List<Part> pp = i2.getParts();
-							if (pp != null) {
-								// multipart download
-								System.out.println("Parts: " + pp.size());
-								for (Part p : pp) {
-									if (p.getState().equals(States.DOWNLOADING)) {
-										parts += String.format("Part#%d(%.2f) ", p.getNumber(),
-												p.getCount() / (float) p.getLength());
-									}
-								}
-							}
-
-							System.out.println(String.format("%s %.2f %s", i1.getState(),
-									i2.getCount() / (float) i2.getLength(), parts));
-						}
-						break;
-					default:
-						break;
-					}
-				}
-			};
-
-			// [OPTIONAL] limit maximum quality, or do not call this function if
-			// you wish maximum quality available.
-			//
-			// if youtube does not have video with requested quality, program
-			// will raise en exception.
-			VGetParser user = null;
-
-			// create proper html parser depends on url
-			// user = VGet.parser(url);
-
-			// download maximum video quality from youtube
-			user = new YouTubeQParser(YoutubeQuality.p360);
-
-			// download mp4 format only, fail if non exist
-			// user = new YouTubeMPGParser();
-
-			// create proper videoinfo to keep specific video information
-			info = user.info(url);
-
-			VGet v = new VGet(info, path);
-
-			// [OPTIONAL] call v.extract() only if you d like to get video title
-			// or download url link
-			// before start download. or just skip it.
-			// v.extract(user, stop, notify);
-
-			System.out.println("Title: " + info.getTitle());
-			// System.out.println("Download URL: " +
-			// info.getInfo().getSource());
-
-			v.download(user, stop, notify);
-		} catch (RuntimeException e) {
-			throw e;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	private static String getUrlSource(URL url) throws IOException {
-		URLConnection yc = url.openConnection();
-		BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream(), "UTF-8"));
-		String inputLine;
-		StringBuilder a = new StringBuilder();
-		while ((inputLine = in.readLine()) != null)
-			a.append(inputLine);
-		in.close();
-
-		return a.toString();
-	}
-
-	private static List<YoutubeVideo> getVideosFromPlaylist(String playlistId) {
-		ArrayList<YoutubeVideo> videoList = new ArrayList<YoutubeVideo>();
+	public static List<DownloadThreadManager.YoutubeVideo> getVideosFromPlaylist(String playlistId) {
+		ArrayList<DownloadThreadManager.YoutubeVideo> videoList = new ArrayList<DownloadThreadManager.YoutubeVideo>();
 		try {
 			if (youtube == null) {
 				List<String> scopes = new ArrayList<String>();
@@ -321,7 +148,7 @@ public class YoutubeDownloadManager {
 
 			while (itemIterator.hasNext()) {
 				PlaylistItem playlistItem = itemIterator.next();
-				videoList.add(new YoutubeVideo(playlistItem.getSnippet().getTitle(),
+				videoList.add(new DownloadThreadManager.YoutubeVideo(playlistItem.getSnippet().getTitle(),
 						playlistItem.getContentDetails().getVideoId()));
 			}
 			return videoList;
@@ -336,21 +163,84 @@ public class YoutubeDownloadManager {
 		return null;
 	}
 
-	static class YoutubeVideo {
-		private String videoId = "";
-		private String videoName = "";
+	//Backup website for mp4 files(vevo) http://youtubeinmp4.com/youtube.php?video=
+	static class YoutubeInMp4Source{
+		private URL MP4Link;
+		
+		public YoutubeInMp4Source(String source){
+			int workableEnd = source.indexOf("\" class=\"downloadMP4");
+			if(workableEnd == -1) return;
+			String urlString = source.substring(source.indexOf("downloadMP4\" href=\"")+19, workableEnd);
+			
+			try{
+				MP4Link = new URL("http://youtubeinmp4.com/"+urlString);
+			}catch(MalformedURLException e){
+				e.printStackTrace();
+			}
+		}
+		
+		public URL getMP4Link() {
+			return MP4Link;
+		}
+	}
+	
+	//KeepVid 
+	static class KeepVidSource{
+		private URL M4ALink;
+		private URL MP4Link;
+		
+		//Dirty way of getting source info
+		public KeepVidSource(String source){
+			int dlPlace = source.indexOf("<div id=\"dl\">", 22000);
+			if(dlPlace == -1) return;
+			String workableSource = source.substring(dlPlace);
+			int workableEndPlace = workableSource.indexOf("<ul class=\"switches\">");
+			if(workableEndPlace == -1) return;
+			workableSource = workableSource.substring(0, workableEndPlace);
 
-		public YoutubeVideo(String name, String id) {
-			videoName = name;
-			videoId = id;
+			try{
+				int linkPlace = workableSource.indexOf(" Download MP4 ");
+			//Gets MP4 link
+				if (linkPlace != -1) {
+					String sub = workableSource.substring(0, linkPlace);
+					if (sub != null) {
+						MP4Link = new URL(sub.substring(sub.lastIndexOf("href=\"") + 6, sub.lastIndexOf("\" class=")));
+					} else {
+						return;
+					}
+
+				} else {
+					System.out.println("Bad website source! Can't find link place!");
+					return;
+				}
+				
+			//Gets M4A link
+				linkPlace = workableSource.indexOf(" Download M4A ");
+				if (linkPlace != -1) {
+					String sub = workableSource.substring(0, linkPlace);
+					if (sub != null) {
+						M4ALink = new URL(sub.substring(sub.lastIndexOf("href=\"") + 6, sub.lastIndexOf("\" class=")));
+					} else {
+						return;
+					}
+
+				} else {
+					System.out.println("Bad website source! Can't find link place!");
+					return;
+				}
+			}catch(MalformedURLException e){
+				System.out.println("Couldn't create download URL");
+				e.printStackTrace();
+			}
 		}
 
-		public String getVideoId() {
-			return videoId;
+		public URL getM4ALink() {
+			return M4ALink;
 		}
 
-		public String getVideoName() {
-			return videoName;
+		public URL getMP4Link() {
+			return MP4Link;
 		}
+
 	}
 }
